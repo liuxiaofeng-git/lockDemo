19aadf8e16836984bc59d240b8283cb6eb40cc9a
liuxiaofeng <1937023747@qq.com>
2020-05-01 21:03:27 +0800

    读写锁初始化

---
+++ lock-readwrite/src/main/java/com/atguigu/lockreadwrite/LockReadwriteApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.lockreadwrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockReadwriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockReadwriteApplication.class, args);
+    }
+
+}
---
+++ lock-readwrite/src/test/java/com/atguigu/lockreadwrite/LockReadwriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.lockreadwrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockReadwriteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
